966b45a0e57273a2650910551ba3f140eb7a6a0e
gagashukla913 <gagan.s@chapter247.com>
2021-11-10 12:54:00 +0530

    remove migrations

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -30,38 +30,7 @@ class PermissionSeeder extends Seeder{
         Permission::create(['name' => 'user-destroy', 'guard_name' => 'admin']);
         Permission::create(['name' => 'user-proxy', 'guard_name' => 'admin']);
 
-        //Inventory Management Admin
-        Permission::create(['name' => 'product-section', 'guard_name' => 'admin']);
-        Permission::create(['name' => 'product-index', 'guard_name' => 'admin']);
-        Permission::create(['name' => 'product-create', 'guard_name' => 'admin']);
-        Permission::create(['name' => 'product-edit', 'guard_name' => 'admin']);
-        Permission::create(['name' => 'product-clone', 'guard_name' => 'admin']);
-        Permission::create(['name' => 'product-url', 'guard_name' => 'admin']);
-        Permission::create(['name' => 'product-destroy', 'guard_name' => 'admin']);
-        Permission::create(['name' => 'product-status', 'guard_name' => 'admin']);
-
-        //Category Management
-        Permission::create(['name' => 'category-section', 'guard_name' => 'admin']);
-        Permission::create(['name' => 'category-index', 'guard_name' => 'admin']);
-        Permission::create(['name' => 'category-create', 'guard_name' => 'admin']);
-
-    
-
         $role = Role::where('name','admin')->first();
         $role->givePermissionTo(Permission::all());
-
-        //Inventory Management User
-        // Permission::create(['name' => 'product-section', 'guard_name' => 'web']);
-        // Permission::create(['name' => 'product-index', 'guard_name' => 'web']);
-        // Permission::create(['name' => 'product-create', 'guard_name' => 'web']);
-        // Permission::create(['name' => 'product-edit', 'guard_name' => 'web']);
-        // Permission::create(['name' => 'product-clone', 'guard_name' => 'web']);
-        // Permission::create(['name' => 'product-url', 'guard_name' => 'web']);
-        // Permission::create(['name' => 'product-destroy', 'guard_name' => 'web']);
-        // Permission::create(['name' => 'product-status', 'guard_name' => 'web']);
-    
-        // $role = Role::where('name','user')->first();
-        // $role->givePermissionTo(Permission::where('guard_name', 'web')->get());
-
     }
 }
